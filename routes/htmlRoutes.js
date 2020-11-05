09d8275a1d1095980342e8d5e0adaed20c60d046
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-11-04 22:59:57 -0500

    added npm package

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
 let notesData = require("../db/db.json");
 const fs = require("fs");
+
+router.get("/notes", function(req, res){
+    res.json(notesData);
+});
